e92a437ed9ca9af5f878c78e36528fa91dab230c
GILBERT-Tuyambaze <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2023-12-09 00:31:51 +0200

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".section", {
+    slidesPerview:1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+   
+  });
